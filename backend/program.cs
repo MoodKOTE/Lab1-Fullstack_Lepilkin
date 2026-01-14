2349d04e394eeb15c436046c2dc39e9c6777ee5a
Лепилкин Максим <maksimlepilkin7@gmail.com>
2026-01-14 21:01:47 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
+Console.WriteLine("Лепилкин Максим Александрович!");
+Console.WriteLine(DateTime.Now);

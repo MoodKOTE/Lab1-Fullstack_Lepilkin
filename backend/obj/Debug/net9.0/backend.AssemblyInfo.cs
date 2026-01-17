b1f098a3a9a986b08f0c2f4078902c1abc2ce95a
Лепилкин Максим <maksimlepilkin7@gmail.com>
2026-01-17 18:13:44 +0300

    r

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69993cd81653916ea356c3723a67961586a2518")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aafa16c4012946d80016e11dac4baba89561736d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
